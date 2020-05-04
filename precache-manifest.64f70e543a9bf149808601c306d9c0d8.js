a3041a7133382e3b1f9cc6656957c00d4c7c5c71
Jack Thompson <jakkuthompson@gmail.com>
2020-05-04 13:45:09 -0400

    Updates

---
+++ precache-manifest.64f70e543a9bf149808601c306d9c0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdcdc0d308f25354e9b9c4022ebd58c",
+    "url": "/gpa/index.html"
+  },
+  {
+    "revision": "dae56bb7559b3113de54",
+    "url": "/gpa/static/css/main.00fef34d.chunk.css"
+  },
+  {
+    "revision": "4d9eafb1ec3309fc31b2",
+    "url": "/gpa/static/js/2.5e2b8834.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gpa/static/js/2.5e2b8834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae56bb7559b3113de54",
+    "url": "/gpa/static/js/main.1bbfe3da.chunk.js"
+  },
+  {
+    "revision": "0a92696b4aea5db70f48",
+    "url": "/gpa/static/js/runtime-main.eefee7af.js"
+  }
+]);
